eea6b1c90d724faad28fcf391da4df1bcfccd72b
Jaroslav Peska <peska.jaroslav@gmail.com>
2013-01-12 13:36:17 +0100

    Updated the copyright notice.

--- src/libi2c.c
+++ src/libi2c.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  * @section LICENSE
  *
- * Copyright (c) 2012 Jaroslav Peska
+ * Copyright (c) 2012 - 2013 Jaroslav Peska
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
